4f3574f0e992b8fe2fdcfb59fe921b8895e17d92
Lorenzo Corbella <lorenzo.corbella@avanade.com>
2023-11-03 16:48:03 +0100

    types for SoundManager

--- src/engine/SoundManager.ts
+++ src/engine/SoundManager.ts
@@ -1,9 +1,9 @@
-import { Sound } from "@pixi/sound";
+import { Sound } from "@pixi/sound";  // https://pixijs.io/sound/examples/index.html
 import gsap from "gsap";
 
 export class SoundManager {
 
-    private sounds = new Map()
+    private sounds = new Map<string, Sound>()
     private globalVolume: number;
 
     constructor(globalVolume: number = 1) {
@@ -25,19 +25,19 @@ export class SoundManager {
 
     playSound(name: string) {
         if (this.sounds.has(name)) {
-            this.sounds.get(name).play({ volume: this.globalVolume });
+            this.sounds.get(name)!.play({ volume: this.globalVolume });
         }
     }
 
     stopSound(name: string) {
         if (this.sounds.has(name)) {
-            this.sounds.get(name).stop();
+            this.sounds.get(name)!.stop();
         }
     }
 
     fadeInSound(name: string, duration: number = 1) {
         if (this.sounds.has(name)) {
-            const sound = this.sounds.get(name)
+            const sound = this.sounds.get(name)!
             sound.play({ volume: 0 });
             gsap.to(sound, { volume: this.globalVolume, duration });
         }
@@ -46,30 +46,30 @@ export class SoundManager {
     fadeOutSound(name: string, volume: number = 0, duration: number = 1) {
         if (this.sounds.has(name)) {
             // this.sounds.get(name).fade(volume, duration);
-            gsap.to(this.sounds.get(name), { volume, duration });
+            gsap.to(this.sounds.get(name)!, { volume, duration });
         }
     }
 
     toggleSound(name: string) {
         if (this.sounds.has(name)) {
             let sound = this.sounds.get(name);
-            if (sound.isPlaying) {
-                sound.stop();
+            if (sound!.isPlaying) {
+                sound!.stop();
             } else {
-                sound.play();
+                sound!.play();
             }
         }
     }
 
     setSoundVolume(name: string, volume: number) {
         if (this.sounds.has(name)) {
-            this.sounds.get(name).volume = volume;
+            this.sounds.get(name)!.volume = volume;
         }
     }
 
     isPlaying(name: string): boolean {
         if (this.sounds.has(name)) {
-            return this.sounds.get(name).isPlaying;
+            return this.sounds.get(name)!.isPlaying;
         }
         return false;
     }
